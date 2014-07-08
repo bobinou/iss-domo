1cc0e5464f8b2df6df7f1906bd9b4f91e033e0d4
bobinou <olivier.parpaillon@gmail.com>
2014-07-08 16:56:57 +0200

    Update xbmc.php

--- app/config/xbmc.php
+++ app/config/xbmc.php
@@ -1,15 +1,33 @@
 <?php
 
-return array(
-
 	/**
 	* Fix the XBMC url and port and options available
 	* You can Enable or Disable Movies or Songs
 	*/
 
-	'xbmc_url' => 'http://192.168.0.26:8080/jsonrpc',
-	'xbmc_movies' => 1,
-	'xbmc_songs' => 1,
+$xbmc_url = 'http://192.168.0.26:8080/jsonrpc';
+$xbmc_movies = 1;
+$xbmc_songs = 1;
+
+//Test if XBMC is UP
+$ch = curl_init($xbmc_url);
+curl_setopt($ch, CURLOPT_NOBODY, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_exec($ch);
+$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+if (200==$retcode) {
+        $xbmc_status = 1;
+} else {
+        $xbmc_status = 0;
+}
+
+return array(
+
+	'xbmc_url' => $xbmc_url,
+	'xbmc_movies' => $xbmc_movies,
+	'xbmc_songs' => $xbmc_songs,
+	'xbmc_status' => $xbmc_status,
 	
     'args-movies' => array(
         'request' => '{"jsonrpc":"2.0","method":"VideoLibrary.GetMovies","id":"libMovies"}'
