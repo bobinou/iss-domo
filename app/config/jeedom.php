a6e6fe81bbce663c25d9d1d9207b6c5126bd8bac
bobinou <olivier.parpaillon@gmail.com>
2014-07-10 16:38:05 +0200

    Create jeedom.php

---
+++ app/config/jeedom.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+	/**
+	* Fix the Jeedom url and port and api key
+	*/
+	
+	'jeedom_url' => 'http://192.168.0.26/jeedom/core/api/jeeApi.php',
+	'api_key' => 'xxxxxxxxxxxxxxxxxxx',
+);
