50f16eef5ff4fbe848069a73588089087a630d09
bobinou <olivier.parpaillon@gmail.com>
2014-10-28 22:35:28 +0100

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,11 @@
 <?php
 //Routes for Imperihome
+Route::get('devices/{deviceId}/action/{actionName}/{actionParam?}', 'DomoticzController@device');
 Route::get('devices', 'DomoticzController@devices');
 Route::get('rooms', 'DomoticzController@rooms');
 Route::get('system', 'DomoticzController@system');
 Route::get('/', 'DomoticzController@system');
 
 //Routes test Jeedom
-Route::get('jeedomroom', 'DomoticzController@getjeedomroom');
-Route::get('jeedomdevice', 'DomoticzController@getjeedomdevice');
-Route::get('jeedomdata', 'DomoticzController@getjeedomdata');
-Route::get('jeedomdatab', 'DomoticzController@getjeedomdataB');
-Route::get('jeedomformat', 'DomoticzController@formatjeedomid');
-Route::get('jeedomtest', 'DomoticzController@jeedomtest');
+Route::get('jeedomroom', 'DomoticzController@getjeedomrooms');
+Route::get('jeedomfull', 'DomoticzController@getjeedomfull');
