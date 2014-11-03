38b07f9ddd93eadd47b32d7b4f9c29ab90a6cd0e
bobinou <olivier.parpaillon@gmail.com>
2014-11-03 08:39:30 +0100

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,15 @@
 <?php
 
-Route::get('devices/{deviceId}/action/{actionName}/{actionParam?}', 'DomoticzController@device');
+Route::get('devices/{deviceId}/action/{actionName}/{actionParam?}', 'DomoticzController@action');
 Route::get('devices', 'DomoticzController@devices');
 Route::get('rooms', 'DomoticzController@rooms');
-//Route::get('action_ret', 'DomoticzController@action_ret');
 Route::get('system', 'DomoticzController@system');
 Route::get('/', 'DomoticzController@system');
+
 Route::get('movies', 'DomoticzController@getmovies');
 Route::get('freebox', 'DomoticzController@getfreebox');
 Route::get('xbmcmovies', 'DomoticzController@getxbmcmovies');
 Route::get('xbmcsongs', 'DomoticzController@getxbmcsongs');
 Route::get('jeedomroom', 'DomoticzController@getjeedomroom');
-Route::get('jeedomdevice', 'DomoticzController@getjeedomdevice');
-Route::get('jeedomdata', 'DomoticzController@getjeedomdata');
-Route::get('jeedomdatab', 'DomoticzController@getjeedomdataB');
+
 Route::get('devices/{deviceId}/{paramKey}/histo/{startdate}/{enddate}', 'DomoticzController@device_history');
